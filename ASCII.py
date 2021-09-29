b9ade6d7f8794d91d15be574d72e7b8aac459430
tariqalmalki <tariqalmalki@gmail.com>
2021-09-29 23:42:19 +0300

    Update ASCII.py

--- ASCII.py
+++ ASCII.py
@@ -1,25 +1,6 @@
-### Demonstrating the ord() function
-##
-##ch1 ='ر' 
-##ch2 = ' ' # space
-##
-##print(ord(ch1))
-##print(ord(ch2))
-##
-### Demonstrating the chr() function
-##
-##print(chr(97))
-##print(chr(945))
-
-
-
-
 lst = []
 for elem in range (1530,1600):
     print(chr(elem))
-##    x = str(elem) + " is " + chr(elem)
-##
-##    lst.append(x)
     
     
     
