e74dfedfae8aac4961b009a3524319d70a597065
tariqalmalki <tariqalmalki@gmail.com>
2021-09-30 00:05:40 +0300

    Add files via upload

---
+++ CheckPlatform.py
@@ -0,0 +1,10 @@
+import platform
+
+print(platform.system())
+print(platform.machine())
+print(platform.processor())
+print(platform.version())
+print(platform.python_implementation())
+print(platform.python_version_tuple())
+print(platform.platform(1))
+print(platform.platform(0, 1))
