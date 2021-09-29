d33c3623ece64abc8a17fa3d1f5b18ee69b70e04
tariqalmalki <tariqalmalki@gmail.com>
2021-09-29 23:30:01 +0300

    Update FibonacciSequenceNumber.py

--- FibonacciSequenceNumber.py
+++ FibonacciSequenceNumber.py
@@ -1,21 +1,3 @@
-##def fib(n):
-##    
-##    if n == 0:
-##         return 0
-##    if n < 3:
-##        return 1
-##
-##    elem1 = elem2 = 1
-##    sum = 0
-##    for i in range(3, n + 1):
-##        sum = elem1 + elem2
-##        elem1, elem2 = elem2, sum
-##    return sum
-##
-##
-##for n in range(0, 10): # testing
-##    print(n, "->", fib(n))
-
 #Fibonacci sequence, such that each number is the sum of the two preceding ones
 
 def fib(n):
